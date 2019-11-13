3f3030432ba9c45eb75cd28968465f2c9f036fc3
Hawke Lansang <hawkeflansang@gmail.com>
2019-11-13 10:27:16 -0500

    final item page updates

--- public/assets/js/itemshop.js
+++ public/assets/js/itemshop.js
@@ -1,49 +1,58 @@
 $(document).ready(function(){
-    var hero = {}
-    $.get("/api/heroChoice/1").then(function (response){
-        hero = response;
-        console.log(hero);
-
-    });
-
-
+    var hero = {};
     var healthItemBtn = $("#healthItem");
     var attackItemBtn = $("#attackItem");
     var armorItemBtn = $("#armorItem");
     var levelItemBtn = $("#levelItem");
-
+    var sendStats = function(){
+        $.get("/api/heroChoice/1").then(function (response){
+            hero = response;
+          $("#heroName").text(response.name);
+          $("#empty-heroSpace").attr("src", response.characterPic);
+          $("#heroLvl").text("Level: "+response.level);
+          $("#heroHP").text("Health: "+response.health);
+          $("#heroAP").text("Attack: "+response.attack);
+          $("#heroBP").text("Block: "+response.block);
+          $("#heroDP").text("Dodge: "+response.dodge);
+          $("#heroCrit").text("Crit Mod: "+response.critMod);
+          $("#heroCoin").text("Coins: "+response.coin);
+          
+        });
+   
+    }
+    sendStats();
     healthItemBtn.on("click", function(){
         $.ajax({
             type: 'PUT',
-            url: '/api/heroLeveledUp/1',
+            url: '/api/heroHealed/1',
             data: {
                 health: (hero.health + 10),
                 coin: (hero.coin - 2)
             },
             success: function () {
                 console.log("Health Buffed");
-
+                sendStats();
             }
         });
     });
     attackItemBtn.on("click", function(){
         $.ajax({
             type: 'PUT',
-            url: '/api/heroLeveledUp/1',
+            url: '/api/heroHoned/1',
             data: {
                 attack: (hero.attack + 10),
                 coin: (hero.coin - 5)
             },
             success: function () {
                 console.log("Hero Strength up");
-
+                sendStats();
             }
         });
     });
     armorItemBtn.on("click", function(){
         $.ajax({
             type: 'PUT',
-            url: '/api/heroLeveledUp/1',
+            url: '/api/heroArmored/1',
             data: {
                 block: (hero.block + 10),
                 dodge: (hero.dodge + 10),
@@ -51,7 +60,7 @@ $(document).ready(function(){
             },
             success: function () {
                 console.log("Hero Defense up");
-
+                sendStats();
             }
         });
     });
@@ -69,7 +78,7 @@ $(document).ready(function(){
             },
             success: function () {
                 console.log("Hero leveled up");
-
+                sendStats();
             }
         });
     });
